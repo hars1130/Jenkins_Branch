00e8fb89af5c06dc29eb9a86fef639c4a439119d
Harsha <harshavardhan.chakka@gmail.com>
2018-08-14 12:54:32 +0530

    c in mast

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
+public class Hello {
    public static void main(String[] args) {
       
       System.out.println("Hello World");
+      System.out.println("Hello from master");
    }
 }
